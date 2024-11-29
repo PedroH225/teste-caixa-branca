a6294d8903d9fd4787c34487941c68cd0e81d484
PedroH225 <pedrodguimaraes20@gmail.com>
2024-11-28 21:40:55 -0300

    Grafo de fluxo

--- src/login/User.java
+++ src/login/User.java
@@ -6,36 +6,64 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class User {
-    public Connection conectarBD() {
-        Connection conn = null;
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-            String url = "jdbc:mysql://localhost/test?user=root&password=password";
-            conn = DriverManager.getConnection(url);
-            
-        } catch (Exception e) { }
-        return conn;
-    }
+	public Connection conectarBD() {
+		/* 4 */ Connection conn = null;
+		/* 5 */ try {
 
-    public String nome = "";
-    public boolean result = false;
+			/* 6 */
+			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+			String url = "jdbc:mysql://localhost/test?user=meu_usuario&password=minha_senha";
 
-    public boolean verificarUsuario(String login, String senha) {
-        String sql = "";
-        Connection conn = conectarBD();
-        // INSTRUÇÃO SQL
-        sql += "select nome from usuarios ";
-        sql += "where login = '" + login + "'";
-        sql += " and senha = '" + senha + "';";
+			/* 7 */
+			conn = DriverManager.getConnection(url);
 
-        try {
-            Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery(sql);
-            if (rs.next()) {
-                result = true;
-                nome = rs.getString("nome");
-            }
-        } catch (Exception e) { }
-        return result;
-    }
+			/* 8 */} 
+		/* 9 */catch (Exception e) {
+			
+		} /* 10 */
+		
+		/* 11 */ return conn;
+	} 
+
+	/* 1 */
+	public String nome = "";
+	public boolean result = false;
+
+	public boolean verificarUsuario(String login, String senha) {
+		/* 2 */ String sql = "";
+		/* 3 */ Connection conn = conectarBD();
+
+		// INSTRUÇÃO SQL
+
+		/* 12 */
+		sql += "select nome from usuarios ";
+		sql += "where login = '" + login + "'";
+		sql += " and senha = '" + senha + "';";
+
+		/* 13 */ try {
+			
+			/* 14 */
+			Statement st = conn.createStatement();
+			
+			/* 15 */
+			ResultSet rs = st.executeQuery(sql);
+			
+			/* 16 */ if (rs.next()) {
+
+				/* 17 */
+				result = true;
+				
+				/* 18 */
+				nome = rs.getString("nome");
+
+			} /* 19 */
+			
+			/* 20 */ } 
+		
+		/* 21 */ catch (Exception e) {
+			
+		} /* 22 */
+		
+		/* 23 */ return result;
+	} 
 }
