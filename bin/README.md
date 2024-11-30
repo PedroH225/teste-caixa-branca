fd7a80b986d6fdc9a6bcef5a09ef7df0c26a1fd3
PedroH225 <pedrodguimaraes20@gmail.com>
2024-11-30 15:29:18 -0300

    Javadoc

---
+++ bin/README.md
@@ -0,0 +1,9 @@
+# Teste caixa branca
+
+## Possíveis erros:
+1. O usuário pode utilizar o driver do mysql incorreto;
+2. A URL do banco de dados pode não condizer com o banco do usuário;
+3. O usuário e senha presentes na URL podem não condizer com o banco do usuário;
+4. O usuário pode escrever um login e senha incorretos;
+5. O código não retorna mensagens de erro;
+6. O código não apresenta mensagens de retorno.
--- src/login/User.java
+++ src/login/User.java
@@ -5,7 +5,27 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+/** 
+ * Classe responsável pela autenticação de usuários.
+ * 
+ * <p>Essa classe fornece métodos para conexão com o banco de dados e login de usuários.</p>
+ *
+ * @author pedroh225
+ * @version 1.0
+ * 
+ */
 public class User {
+	
+	/**
+	 * Realiza a conexão com o banco de dados.
+	 * 
+	 * @value conn Instância da conexão com o banco de dados.
+	 * @value url A url do banco de dados passando o usuário e senha.
+	 * 
+	 * @return conn A instância da conexão com o banco de dados.
+	 * 
+	 * @throws SQLException Caso haja um erro na conexão com o banco.
+	 */
     public Connection conectarBD() {
         Connection conn = null;
         try {
@@ -19,7 +39,25 @@ public class User {
 
     public String nome = "";
     public boolean result = false;
-
+    
+    
+    /**
+	 * Faz a autenticação do usuário.
+	 * 
+	 * @param login O login do usuário a ser autenticado.
+	 * @param senha A senha do usuário a ser autenticado.
+	 * 
+	 * @value conn Instância da conexão com o banco de dados.
+	 * @value sql String da consulta do banco de dados.
+	 * @value st Reponsável por criar a consulta do banco.
+	 * @value rs Responsável por armazenar o resultado da consulta do banco.
+	 * @value result Responsável por atribuir o estado da autenticação do usuário
+	 * @value nome Atributo da classe User, responsável por armazenar a string da coluna "nome" do banco de dados.
+	 * 
+	 * @return result True caso o usuário for autenticado com sucesso, caso contrário, retorna False.
+	 * 
+	 * @throws SQLException Caso haja um erro com a consulta ao banco de dados.
+	 */
     public boolean verificarUsuario(String login, String senha) {
         String sql = "";
         Connection conn = conectarBD();
